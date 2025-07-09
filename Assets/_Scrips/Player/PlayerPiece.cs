ed7828cece095056d47f578634438e0bc2d7d1e4
Nam <vuhoangnam310104@gmail.com>
2025-07-09 16:58:25 +0700

    move position and rotation

--- Assets/_Scrips/Player/PlayerPiece.cs
+++ Assets/_Scrips/Player/PlayerPiece.cs
@@ -1,11 +1,13 @@
-using System;
+﻿using System;
 using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerPiece : MonoBehaviour
 {
     public Node currentNode;
     public float speed = 5f;
+    float turnSpeedMultiplier = 8f;
     public bool isMoving = false;
     public Animator animator;
 
@@ -18,6 +20,10 @@ public class PlayerPiece : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Alpha3)) MoveStep(3);
             if (Input.GetKeyDown(KeyCode.Alpha4)) MoveStep(4);
             if (Input.GetKeyDown(KeyCode.Alpha5)) MoveStep(5);
+            if (Input.GetKeyDown(KeyCode.Alpha6)) MoveStep(6);
+            if (Input.GetKeyDown(KeyCode.Alpha7)) MoveStep(7);
+            if (Input.GetKeyDown(KeyCode.Alpha8)) MoveStep(8);
+            if (Input.GetKeyDown(KeyCode.Alpha9)) MoveStep(9);
         }
     }
 
@@ -33,22 +39,30 @@ public class PlayerPiece : MonoBehaviour
 
         for (int i = 0; i < steps; i++)
         {
-            if (currentNode.nextNode == null)
-                break;
+            if (currentNode.nextNode == null) break;
 
             Node nextNode = currentNode.nextNode;
             Vector3 startPosition = transform.position;
             Vector3 endPosition = nextNode.transform.position + Vector3.up * 0.5f;
+            Vector3 direction = (endPosition - startPosition).normalized;
+
+            Quaternion startRotation = transform.rotation;
+            Quaternion endRotation = Quaternion.LookRotation(direction);
 
             float distance = Vector3.Distance(startPosition, endPosition);
             float duration = distance / speed;
-            float elapsedTime = 0f;
+            float timer = 0f;
 
-            while (elapsedTime < duration)
+            while (timer < duration)
             {
-                float t = elapsedTime / duration;
+                float t = timer / duration;
+                float turnT = Mathf.Clamp01(t * turnSpeedMultiplier);
+
                 transform.position = Vector3.Lerp(startPosition, endPosition, t);
-                elapsedTime += Time.deltaTime;
+
+                transform.rotation = Quaternion.Slerp(startRotation, endRotation, turnT);
+
+                timer += Time.deltaTime;
                 yield return null;
             }
 
@@ -58,5 +72,4 @@ public class PlayerPiece : MonoBehaviour
         animator.Play("Idle01");
         isMoving = false;
     }
-
 }
